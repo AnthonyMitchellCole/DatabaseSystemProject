29f0f493f52ebcdd7aa0b8b44cc37ff31a7dd38b
anthony.mitchell.cole@gmail.com <anthony.mitchell.cole@gmail.com>
2024-04-14 17:54:29 -0600

    Adding Select2 & Bootstrap theme & styling dropdowns

--- public/sw.js
+++ public/sw.js
@@ -7,7 +7,12 @@ self.addEventListener('install', function(event) {
                 '/main.js',
                 '/bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js',
                 '/bootstrap-5.3.3-dist/css/bootstrap.min.css',
-                'https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,wght@8..144,100..1000&display=swap'
+                'https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,wght@8..144,100..1000&display=swap',
+                'https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css',
+                'https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js',
+                'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js',
+                'https://code.jquery.com/jquery-3.7.1.min.js',
+                'https://cdn.jsdelivr.net/npm/@ttskch/select2-bootstrap4-theme@x.x.x/dist/select2-bootstrap4.min.css'
             ]);
         })
     );
